a2d7554d1022ef0ab911d5aa4056079e32bf75f8
Katy Moe <katy@katy.moe>
2016-07-26 22:51:26 +0100

    poke.pokemon.PokemonId

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ var username = process.env.PGO_USERNAME;
 var password = process.env.PGO_PASSWORD;
 var provider = process.env.PGO_PROVIDER || 'google';
 
+var pokeMap;
+
 a.init(username, password, location, provider, function(err) {
   if (err) throw err;
 
@@ -36,40 +38,57 @@ a.init(username, password, location, provider, function(err) {
     console.log('1[i] Pokecoin: ' + poke);
     console.log('1[i] Stardust: ' + profile.currency[1].amount);
 
-    setInterval(function(){
-      a.Heartbeat(function(err,hb) {
+
+    setInterval(function() {
+
+      a.Heartbeat(function (err,hb) {
         if(err) {
           console.error(err);
         }
 
         for (var i = hb.cells.length - 1; i >= 0; i--) {
-          if(hb.cells[i].NearbyPokemon[0]) {
-            // console.log(a.pokemonlist[0])
+          if(hb.cells[i].WildPokemon[0]) {
 
             var wildPokemon = hb.cells[i].WildPokemon;
 
-            console.log(wildPokemon);
-
             for (var j = wildPokemon.length - 1; j >= 0; j--) {
-              var pokemon = a.pokemonlist[parseInt(wildPokemon[j].pokemon.PokemonId)-1];
-              var latitude = wildPokemon[j].Latitude;
-              var longitude = wildPokemon[j].Longitude;
-              console.log(pokemon);
+              var pokeId = wildPokemon[j].pokemon.PokemonId;
+              var pokemon = a.pokemonlist[parseInt(pokeId)-1];
 
-              var message = 'There is a ' + pokemon.name + ' nearby! Map: https://www.google.co.uk/maps/@' + latitude + ',' + longitude + ',14z?hl=en';
-
-              request.post({
-                url: process.env.SLACK_WEBHOOK_URL,
-                json: true,
-                body: {
-                  text: message,
-                  icon_url: pokemon.img
-                }
-              }, function(error, response, body) {
-                console.error(error);
-                console.log(response.body);
+              var pokemonAlreadyPresent = _.some(pokeMap, function(poke) {
+                console.log(poke.id);
+                console.log(pokeId);
+                return poke.id === pokeId;
               });
+
+              if (!pokemonAlreadyPresent) {
+                var latitude = wildPokemon[j].Latitude;
+                var longitude = wildPokemon[j].Longitude;
+
+                var message = 'There is a ' + pokemon.name + ' nearby! Map: https://www.google.co.uk/maps/@' + latitude + ',' + longitude + ',14z?hl=en';
+                console.log(pokemon.name + ' detected');
+
+                request.post({
+                  url: process.env.SLACK_WEBHOOK_URL,
+                  json: true,
+                  body: {
+                    text: message,
+                    icon_url: pokemon.img
+                  }
+                }, function(error, response, body) {
+                  console.error(error);
+                  console.log(response.body);
+                });
+              } else {
+               console.log(pokemon.name + ' already present: skipping');
+              }
             }
+
+            pokeMap = _.map(wildPokemon, function(poke) {
+              return {
+                id: poke.pokemon.PokemonId
+              };
+            });
           }
         }
 
