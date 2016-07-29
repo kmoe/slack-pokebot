910e97cb5f96a928f94b0a5ed53140c931318af6
Katy Moe <katy@katy.moe>
2016-07-29 21:26:49 +0100

    lint geo.js

--- geo.js
+++ geo.js
@@ -9,48 +9,21 @@ function getDistance(a, b) {
 }
 
 function reverseGeoCode(location, callback) {
-  geocoder.reverseGeocode(location.latitude, location.longitude,
-   function (err, data) {
-     if (data && data.results && data.results[0]) {
-       callback(' (' + data.results[0].formatted_address + ')');
-     } else {
-       callback('');
-     }
-   });
+  geocoder.reverseGeocode(location.latitude, location.longitude, (err, data) => {
+    if (data && data.results && data.results[0]) {
+      callback(` (${data.results[0].formatted_address})`);
+    } else {
+      callback('');
+    }
+  });
 }
 
-function radians(degrees) {
-  return degrees * Math.PI / 180;
+function radians(deg) {
+  return deg * Math.PI / 180;
 }
 
-function degrees(radians) {
-  return radians * 180 / Math.PI;
-}
-
-function getBearing(start, target) {
-  const s = toRadians(start);
-  const d = toRadians(target);
-  let dLong = d.longitiude - s.longitiude;
-  const inner =
-      Math.tan(d.latitude / 2.0 + Math.PI / 4.0) /
-      Math.tan(s.latitude / 2.0 + Math.PI / 4.0);
-  const dPhi = Math.log(inner);
-  if (Math.abs(dLong) > Math.PI) {
-    if (dLong > 0.0)
-      dLong = -(2 * Math.PI - dLong);
-    else
-      dLong = (2 * Math.PI + dLong);
-  }
-  const bearing = degrees(Math.atan2(dLong, dPhi));
-  return bearing;
-}
-
-const cardinals = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
-function cardinalBearing(degrees) {
-  const degreesPerItem = 360 / cardinals.length;
-  const option = (degrees + degreesPerItem * 0.5) / degreesPerItem;
-  const index = Math.floor(option + cardinals.length) % cardinals.length;
-  return cardinals[index];
+function degrees(rad) {
+  return rad * 180 / Math.PI;
 }
 
 function toRadians(a) {
@@ -58,6 +31,34 @@ function toRadians(a) {
     latitude: radians(a.latitude),
     longitiude: radians(a.longitude),
   };
+}
+
+
+function getBearing(start, target) {
+  const s = toRadians(start);
+  const d = toRadians(target);
+  let dLong = d.longitiude - s.longitiude;
+  const inner =
+    Math.tan(d.latitude / 2.0 + Math.PI / 4.0) /
+    Math.tan(s.latitude / 2.0 + Math.PI / 4.0);
+  const dPhi = Math.log(inner);
+  if (Math.abs(dLong) > Math.PI) {
+    if (dLong > 0.0) {
+      dLong = -(2 * Math.PI - dLong);
+    } else {
+      dLong = (2 * Math.PI + dLong);
+    }
+  }
+  const bearing = degrees(Math.atan2(dLong, dPhi));
+  return bearing;
+}
+
+const cardinals = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
+function cardinalBearing(deg) {
+  const degreesPerItem = 360 / cardinals.length;
+  const option = (deg + degreesPerItem * 0.5) / degreesPerItem;
+  const index = Math.floor(option + cardinals.length) % cardinals.length;
+  return cardinals[index];
 }
 
 module.exports = {
